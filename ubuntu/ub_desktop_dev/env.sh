fe55ce7d46643825a7313fcdcd879a7a3dc551f6
borendev <157404740+BorenDev@users.noreply.github.com>
2024-12-08 09:35:53 -0700

    latest

---
+++ ubuntu/ub_desktop_dev/env.sh
@@ -0,0 +1,28 @@
+# This environment file contains environment values for shell support scripts.
+#
+# Example values
+# CONTAINER_TOOL=podman
+# CONTAINER_IMAGE_NAME=ub_desktop_dev
+# REMOTE_REGISTRY="<registry_url>:5000/"
+# FOLDER="ub_desktop_dev/"
+# MAJOR_VER=0
+# MINOR_VER=$MAJOR_VER.1
+
+CONTAINER_TOOL=podman
+CONTAINER_IMAGE_NAME=ub_desktop_dev
+REMOTE_REGISTRY=""
+FOLDER="ubuntu_dev/"
+MAJOR_VER=0
+MINOR_VER=1
+BUILD_VER=1
+MAJ_MIN_VER=$MAJOR_VER.$MINOR_VER
+D_VERSION=$MAJOR_VER.$MINOR_VER.$BUILD_VER
+
+# User
+USER_NAME=developer
+USER_ID=1000
+GROUP_ID=1000
+
+WORKDIR=/workdir
+
+# Build ARG vars
